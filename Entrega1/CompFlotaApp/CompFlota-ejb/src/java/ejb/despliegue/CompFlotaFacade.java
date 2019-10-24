a9659cd99ef926aeb1b84bbab5fc0c5e05a24d3d
Pablo Renero Balgañón <prenero4bc@gmail.com>
2019-10-24 20:50:24 +0200

    Añadido método getVehiculos sin implementar

--- Entrega1/CompFlotaApp/CompFlota-ejb/src/java/ejb/despliegue/CompFlotaFacade.java
+++ Entrega1/CompFlotaApp/CompFlota-ejb/src/java/ejb/despliegue/CompFlotaFacade.java
@@ -9,6 +9,8 @@ import ejb.dominio.Modelo;
 import ejb.persistencia.VehiculoFacadeLocal;
 import ejb.dominio.Vehiculo;
 import ejb.persistencia.ModeloFacadeLocal;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -29,20 +31,24 @@ public class CompFlotaFacade implements CompFlotaFacadeLocal {
     @EJB
     private ModeloFacadeLocal modeloFacade;
 
+    public List<Vehiculo> getVehiculos(String[] licencias, Date fechaIni, Date fechaFin) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public boolean addVehiculo(String idModelo, String matricula, String color, float km, char averiado) {
         LOGGER.log(Level.INFO, "Llamada a addVehiculo");
         if (idModelo == null || idModelo.isEmpty() || matricula == null || matricula.isEmpty() || color == null || color.isEmpty() || km < 0 || (averiado != 'T' && averiado != 'F')) {
             return false;
         }
         Modelo modelo = modeloFacade.find(idModelo);
-        if ( modelo == null) {
+        if (modelo == null) {
             return false;
         }
         Vehiculo vehiculo = new Vehiculo(matricula, color, km, averiado);
         vehiculo.setIdmodelo(modelo);
-        try{
-        vehiculoFacade.create(vehiculo);
-        }catch(EntityExistsException e){
+        try {
+            vehiculoFacade.create(vehiculo);
+        } catch (EntityExistsException e) {
             return false;
         }
         return true;
--- Entrega1/CompFlotaApp/CompFlota-ejb/src/java/ejb/despliegue/CompFlotaFacadeLocal.java
+++ Entrega1/CompFlotaApp/CompFlota-ejb/src/java/ejb/despliegue/CompFlotaFacadeLocal.java
@@ -5,6 +5,9 @@
  */
 package ejb.despliegue;
 
+import ejb.dominio.Vehiculo;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -13,23 +16,27 @@ import javax.ejb.Local;
  */
 @Local
 public interface CompFlotaFacadeLocal {
-    
+
+    List<Vehiculo> getVehiculos(String[] licencias, Date fechaIni, Date fechaFin);
+
     /**
      * Añade un vehículo al sistema
+     *
      * @param idModelo
      * @param matricula
      * @param color
      * @param km
      * @param averiado
-     * @return 
+     * @return
      */
-    boolean addVehiculo (String idModelo, String matricula, String color, float km, char averiado);
+    boolean addVehiculo(String idModelo, String matricula, String color, float km, char averiado);
+
     /**
      * Elimina un vhículo del sistema.
      *
      * @param matricula matrícula del vehículo que se quiere borrar.
-     * @return true si el vehículo se ha borrado correctamente, false en cualquier otro caso.
-     * contrario.
+     * @return true si el vehículo se ha borrado correctamente, false en
+     * cualquier otro caso. contrario.
      */
-    boolean delVehiculo (String matricula);
+    boolean delVehiculo(String matricula);
 }
