95159e0a7ebec6a3ba7a925ea7a96d70b3a2ee67
Ivan <ivan.gonzalez.rincon>
2019-10-30 18:14:43 +0100

    Se ha añadido el código por defecto de los facades y las entities para el componente CompReservasAlquiler

---
+++ Entrega1/CompReservasAlquilerApp/CompReservasAlquiler-ejb/src/java/ejb/persistencia/ReservaFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.persistencia;
+
+import ejb.dominio.Reserva;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ivan
+ */
+@Stateless
+public class ReservaFacade extends AbstractFacade<Reserva> implements ReservaFacadeLocal {
+    @PersistenceContext(unitName = "CompReservasAlquiler-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ReservaFacade() {
+        super(Reserva.class);
+    }
+    
+}
