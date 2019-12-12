4a7f862375b5c5fa5b5e33ba9ef9304486813c76
Pablo Renero Balgañón <prenero4bc@gmail.com>
2019-12-12 20:40:18 +0100

    Estructura básica del login

--- Entrega3/cliente/CarClient/src/app/app.module.ts
+++ Entrega3/cliente/CarClient/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
