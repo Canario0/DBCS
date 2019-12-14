76f07e568764adb734b70d77ecc3e2c25374ab00
Pablo Renero Balgañón <prenero4bc@gmail.com>
2019-12-14 18:18:51 +0100

    Añadida interfaz de una reserva

---
+++ Entrega3/cliente/CarClient/src/app/shared/reserva.ts
@@ -0,0 +1,9 @@
+export interface Reserva {
+  id: number;
+  fechaReserva: Date;
+  fechaInicioAlquiler: Date;
+  fechaFinAlquiler: Date;
+  ejecutada: string;
+  nif: string;
+  matricula: string;
+}
