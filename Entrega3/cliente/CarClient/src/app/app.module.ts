29afdcaa4fd8799802489d4f3507f45f9c92d9a3
Pablo Renero Balgañón <prenero4bc@gmail.com>
2019-12-15 11:31:08 +0100

    Añadido componente lista reservas sin las llamadas a la api para borrar y editar

--- Entrega3/cliente/CarClient/src/app/app.module.ts
+++ Entrega3/cliente/CarClient/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSnackBarModule } from '@angular/material';
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSnackBarModule, MatTableModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { ReservasListComponent } from './reservas-list/reservas-list.component';
 
@@ -26,7 +26,8 @@ import { ReservasListComponent } from './reservas-list/reservas-list.component';
     MatButtonModule,
     FormsModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Entrega3/cliente/CarClient/src/app/reservas-list/reservas-list.component.ts
+++ Entrega3/cliente/CarClient/src/app/reservas-list/reservas-list.component.ts
@@ -1,3 +1,5 @@
+import { RESERVAS } from './../shared/Reserva-mock';
+import { Reserva } from './../shared/reserva';
 import { Router } from '@angular/router';
 import { SessionService } from './../shared/session.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,13 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservas-list.component.css']
 })
 export class ReservasListComponent implements OnInit {
+  reservas: Reserva[] = RESERVAS;
+  displayedCols = ['id', 'fechaReserva', 'fechaInicioAlquiler', 'fechaFinAlquiler', 'ejecutada', 'nif', 'matricula', 'actions'];
 
   constructor(private session: SessionService, private route: Router) { }
 
   ngOnInit() {
-    if (this.session.checkLoggedIn() === false) {
-      this.route.navigate(['/login']);
-    }
+    // if (this.session.checkLoggedIn() === false) {
+    //   this.route.navigate(['/login']);
+    // }
   }
 
+  editAction(id: number){
+    console.log(`Me han editado ${id}`);
+  }
+
+  deleteAction(id: number){
+    console.log(`Me han borrado ${id}`);
+  }
 }
