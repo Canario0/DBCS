e2fe6fab69919432b1fec176e2a1c27f4540813d
Ivan <ivan.gonzalez.rincon>
2019-12-15 13:18:15 +0100

    Servicio para obtener todas las reservas de un usuario

--- Entrega3/servicio/DeployWebApplicaction/src/java/backend/SessionService.java
+++ Entrega3/servicio/DeployWebApplicaction/src/java/backend/SessionService.java
@@ -1,8 +1,12 @@
 package backend;
 
+import dominio.Alquiler;
 import dominio.Cliente;
+import dominio.Reserva;
 import dominio.Tipocarnet;
 import dominio.Usuario;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ws.rs.core.Context;
@@ -11,10 +15,13 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import persistencia.AlquilerFacadeLocal;
 import persistencia.ClienteFacadeLocal;
+import persistencia.ReservaFacadeLocal;
 import persistencia.UsuarioFacadeLocal;
 
 /**
@@ -29,12 +36,20 @@ public class SessionService {
     private UsuarioFacadeLocal usuarioFacade;
     @EJB
     private ClienteFacadeLocal clienteFacade;
+    @EJB
+    private AlquilerFacadeLocal alquilerFacade;
+
+    @EJB
+    private ReservaFacadeLocal reservaFacade;
+
+    
     @Context
     private UriInfo context;
 
     private final String CORRECTO = "Login correcto";
     private final String INCORRECTO = "Usuario incorrecto";
     private final String BLOQUEADO = "Usuario bloqueado";
+    private final String NIFINCORRECTO = "Nif incorrecto";
 
     public SessionService() {
 
@@ -82,6 +97,111 @@ public class SessionService {
         }
     }
 
+     @GET
+    @Path("/{userNif}/reserva")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReservasUsuario(@PathParam("userNif") String userNif) {
+
+        List<Reserva> reservas = getReservas(userNif);
+        System.out.println("Estoy cogiendo las reservas");
+        if (reservas != null) {
+            GenericEntity<List<Reserva>> entity = 
+            new GenericEntity<List<Reserva>>(reservas) {};
+            return Response.status(Response.Status.OK)
+                    .entity(entity)
+                    .build();
+
+        } else {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("{ \"message\": \"" + NIFINCORRECTO + "\"}")
+                    .build();
+        }
+    }
+
+    public boolean addReserva(Date fechaInicio, Date fechaFin, String nif, String matricula) {
+        if (fechaInicio == null) {
+            return false;
+        } else if (fechaFin == null) {
+            return false;
+        } else if (nif == null || "".equals(nif.trim())) {
+            return false;
+        } else if (matricula == null || "".equals(matricula.trim())) {
+            return false;
+        }
+        Reserva r = new Reserva();
+        r.setIdreserva(reservaFacade.count() + 1);
+        r.setFechareserva(getToday());
+        r.setFechainicioalquiler(fechaInicio);
+        r.setFechafinalquiler(fechaFin);
+        r.setNif(nif);
+        r.setMatricula(matricula);
+        r.setEjecutada('F');
+        reservaFacade.create(r);
+        if (reservaFacade.find(r.getIdreserva()) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public List<Reserva> getReservas(String nif) {
+        if (nif == null || "".equals(nif.trim())) {
+            return null;
+        }
+        return reservaFacade.findByNif(nif);
+    }
+
+    public boolean addAlquiler(int reserva, float km, String idEmpleado) {
+        if (reserva <= 0) {
+            return false;
+        } else if (km < 0) {
+            return false;
+        } else if (idEmpleado == null || "".equals(idEmpleado.trim())) {
+            return false;
+        }
+        Reserva r = reservaFacade.find(reserva);
+        if (r == null) {
+            return false;
+        } else if (r.getEjecutada() == 'T') {
+            return false;
+        }
+        Alquiler a = new Alquiler();
+        a.setIdalquiler(alquilerFacade.count() + 1);
+        a.setFechainicio(getToday());
+        Date dt = getToday();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
+        c.add(Calendar.DATE, 10);
+        dt = c.getTime();
+        a.setFechafin(dt);
+        a.setKilometrajesalida(km);
+        a.setCliente(r.getNif());
+        a.setMatricula(r.getMatricula());
+        a.setRealizadopor(idEmpleado);
+        r.setEjecutada('T');
+        reservaFacade.edit(r);
+        if (reservaFacade.find(r.getIdreserva()).getEjecutada() == 'F') {
+            return false;
+        }
+        alquilerFacade.create(a);
+        if (alquilerFacade.find(a.getIdalquiler()) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public String[] getReservados(Date fechaInicial, Date fechaFinal) {
+        if (fechaInicial == null) {
+            return null;
+        } else if (fechaFinal == null) {
+            return null;
+        }
+        return reservaFacade.findInDate(fechaInicial, fechaFinal);
+    }
+
+    private Date getToday() {
+        return Calendar.getInstance().getTime();
+    }
     private boolean controlAccesos(String nombre, String clave, String tipoUsuario) {
 
         Boolean valid = false;
